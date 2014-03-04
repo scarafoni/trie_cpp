2c8ace44ba6000e1ecf7adb08345c5bd8b88ed97
scarafoni <dscarafo@u.rochester.edu>
2014-03-04 00:44:52 -0500

    working insertion, deletion

--- trie.h
+++ trie.h
@@ -12,21 +12,134 @@ private:
 	class Trie_Node
 	{
 	private:
-		std::map<char,Trie_Node> children;
+		std::map<char,Trie_Node*> children;
 	public:
 		T  data;
 		bool is_end;
-		void lookup_child(char index, std::string data)
+		Trie_Node(){}
+		bool lookup_child(char index, std::string data)
 		{
-			if(children.find(string[index]) != letter.end())
-				return lookup_child(children[letter],
+			//current letter
+			char cl = data[index];
+			//std::cout << "is_end?- "<<is_end<<" end of string" << index << "\n";
+
+			//if we're at the end, return
+			if(index == data.length())
+			{
+				if(is_end)
+				{
+					std::cout << "end, word found- "<<data<<"\n";
+					return true;
+				}
+				else
+				{
+					std::cout << "end, word not found - "<<data<<"\n";
+					is_end = true;
+					return false;
+				}
+			}
+			//if the node below us is null, then it's not in the system
+			else
+			{
+				if(children.find(cl) == children.end())
+					return false;
+	
+				std::cout << "need to go deeper on- "<<cl<<"\n";
+				return children[cl]->lookup_child(index+1,data);
+			}
+		}
+		bool add_node(char index, std::string data)
+		{
+			//current letter
+			char cl = data[index];
+			//std::cout << "is_end?- "<<is_end<<" end of string" << index << "\n";
+
+			//if we're at the end, return
+			if(index == data.length())
+			{
+				if(is_end)
+				{
+					std::cout << "end, word found- "<<data<<"\n";
+					return true;
+				}
+				else
+				{
+					std::cout << "end, word not found - "<<data<<"\n";
+					is_end = true;
+					return false;
+				}
+			}
+			//if the node below us is null, make it
+			else
+			{
+				if(children.find(cl) == children.end())
+				{
+					std::cout << "made a baby on- "<<cl<<"\n";
+					children[cl] = new Trie_Node;	
+				}
+				std::cout << "need to go deeper on- "<<cl<<"\n";
+				return children[cl]->add_node(index+1,data);
+			}
+		}
+
+		bool delete_node(char index, std::string data)
+		{
+			//current letter
+			char cl = data[index];
+
+			//if we're at the end, return
+			if(index == data.length())
+			{
+				if(is_end)
+				{
+					std::cout << "end, word found- "<<data<<"\n";
+					return true;
+				}
+				else
+				{
+					std::cout << "end, word not found - "<<data<<"\n";
+					is_end = true;
+					return false;
+				}
+			}
+			//if the node below us is null, then it's not in the system
+			else
+			{
+				if(children.find(cl) == children.end())
+					return false;
+	
+				std::cout << "need to go deeper on- "<<cl<<"\n";
+				if(children[cl]->delete_node(index+1,data))
+				{
+					std::cout << "deleteing on-  "<<cl<<"\n";
+					delete(children[cl]);
+					children.erase(cl);
+					return true;
+				}
+				return false;
+			}
 		}
 	};
 
 	Trie_Node* head_node;
 	
 public:
-	T get_head_data(){return head_node.data;}
+	bool lookup(std::string data)
+		{return head_node->lookup_child(0,data);}
+	bool add_node(std::string data)
+		{return head_node->add_node(0,data);}
+	bool delete_node(std::string data)
+	{
+		//if(lookup(data))
+			return head_node->delete_node(0,data);
+		//else
+			//return false;
+	}
+
+	Trie()
+	{
+		head_node = new Trie_Node();
+	}
 };
 
 #endif // __TRIE_H__
--- trie_test.cc
+++ trie_test.cc
@@ -8,14 +8,33 @@ using namespace std;
 int main(void)
 {
 	Trie<string> starter;
-	std::map<char,int> mymap;
-	mymap['c'] = 44;
-	mymap['a'] = 22;
+	string meat = "meat";
+	cout << "word- meat\n";
+	bool has_meat = starter.add_node(meat);
+	cout << has_meat << "\n";
+	has_meat = starter.lookup(meat);
+	cout << has_meat << "\n";
 
-	if(mymap.find('a') != mymap.end())
-		cout << mymap['a'] << "\n";
-	else
-		cout << "error" << "error\n";
+	string meap = "meap";
+	cout << "word- meap\n";
+	bool has_meap = starter.add_node(meap);
+	cout << has_meap << "\n";
+	cout << "lookup after add\n";
+	has_meap = starter.lookup(meap);
+	cout << has_meap << "\n";
+	cout << "lookup after delete\n";
+	starter.delete_node(meap);
+	has_meap = starter.lookup(meap);
+	cout << has_meap << "\n";
+
+	/*
+	string dinner = "dinner";
+	cout << "word- dinner\n";
+	bool has_dinner = starter.lookup(dinner);
+	cout << has_dinner << "\n";
+	has_dinner = starter.lookup(dinner);
+	cout << has_dinner << "\n";
+	*/
 
 	cout << "testing over\n";
 	return 0;
